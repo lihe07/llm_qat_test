423b4cbb7bca8b01112c8ff4df866ba7bec34136
lihe07 <li@imlihe.com>
2025-09-15 13:13:17 +0100

    new data

--- attack_eval.py
+++ attack_eval.py
@@ -1,3 +1,5 @@
+import dataclasses
+import json
 import squad
 from transformers import GPT2ForQuestionAnswering
 import gpt2
@@ -9,22 +11,33 @@ model = GPT2ForQuestionAnswering.from_pretrained(
 )
 
 
-# policies.OutlierAdaptivePolicy(),
-# policies.AggressiveLowBitLoRA(total_layers=12),
-# policies.DepthAdaptivePolicy(total_layers=12),
-# policies.ConservativeLoRAPolicy(full_bias=True),
-# policies.UniformLoRAPolicy(8, 8, 32, True, 8),
-# policies.UniformLoRAPolicy(6, 6, 32, True, 6),
-# policies.UniformLoRAPolicy(4, 4, 32, True, 4),
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)  # type:ignore
+        return super().default(o)
+
+
+p = [
+    policies.UniformLoRAPolicy(4, 4, 32, True, 4),
+]
+
+
+policy_dicts = [gpt2.get_policy_dict(model, policy) for policy in p]
+
 
 gpt2.apply_qat_to_gpt2(
     model,
-    policies.OutlierAdaptivePolicy(),
+    policies.UniformLoRAPolicy(8, 8, 32, True, 8),
+    # policies.OutlierAdaptivePolicy(),
 )
+
 
 model.load_state_dict(
     safetensors.torch.load_file("./gpt2-squad-cascade/model.safetensors"),
     strict=False,
 )
+
+gpt2.apply_qat_to_gpt2(model, policy_dicts[0])
 
 squad.eval_attack(model)
